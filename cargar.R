e8f55841d3cfc25e6688655519b303655af2e278
Max Medina <max.medina@twenty57.com>
2017-12-06 22:24:22 -0400

    modify collection names

--- cargar.R
+++ cargar.R
@@ -1,20 +1,25 @@
 library(mongolite)
 STR_BASE_DATOS = "azucarDB"
 
-ficheros = c('Campana.csv', 'Sesion.csv', 'Mediciones.csv', 'Lote.csv', 'Planton.csv', 'PuntoCaptacion.csv')
-collecciones = c('campanas', 'sesiones', 'mediciones', 'lotes', 'plantones', 'puntocaptaciones')
 nombres = c('campana', 'sesion', 'medicion', 'lote', 'planton', 'puntocaptacion')
 
-cargarFicheroABaseDeDatos <- function(ruta, coleccion, nombre) {
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}
+
+cargarFicheroABaseDeDatos <- function(ruta, nombre) {
   fichero <- read.csv(ruta, header=TRUE)
-  colnames(fichero)[1] <- "cod"
-  myMongo <- mongo(db=STR_BASE_DATOS, coleccion)
+  colnames(fichero)[1] <- paste("id", simpleCap(nombre), sep="")
+  myMongo <- mongo(db=STR_BASE_DATOS, paste(nombre, "s", sep=""))
   myMongo$insert(fichero)
 }
 
 cargarTodo <- function(RutaDirectorio) {
-  for(i in 1:length(ficheros)) {
-    ruta = paste(RutaDirectorio, ficheros[i], sep="/")
-    cargarFicheroABaseDeDatos(ruta, collecciones[i], nombres[i])
+  for(i in 1:length(nombres)) {
+    ruta = paste(RutaDirectorio, paste(nombres[i], "csv", sep="."), sep="/")
+    print(ruta)
+    cargarFicheroABaseDeDatos(ruta, nombres[i])
   }
 }
