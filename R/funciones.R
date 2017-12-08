f048d0ef13de56aec65ca8391a6f912a9b57605c
danielsantil <daniel.shcf@gmail.com>
2017-12-08 00:31:49 -0400

    agregada funcion de backup y documentacion

--- R/funciones.R
+++ R/funciones.R
@@ -9,12 +9,15 @@ cargarData <- function(ruta) {
   dataLote        <- read.csv( file=paste(ruta, 'Lote.csv', sep="/"))
   dataPlanton     <- read.csv( file=paste(ruta, 'Planton.csv', sep="/"))
   dataPunto       <- read.csv( file=paste(ruta, 'PuntoCaptacion.csv', sep="/"))
-  
+
   data <- list(dataCampana, dataSesion, dataMediciones, dataLote, dataPlanton, dataPunto)
   names(data) <- c("campana", "sesion","mediciones", "lote", "planton", "puntoCaptacion")
   data
 }
 
+#' Aplica filtro sobre la data especificada
+#' @param data Data a filtrar
+#' @param filterExp Filtro a aplicar
 filtro <- function (data, filterExp) {
   subset(data, filterExp)
 }
@@ -23,6 +26,11 @@ calcularDistanciaEntreDosPuntos <- function(p1, p2) {
   distHaversine(p1, p2)
 }
 
+#' Calcula la distancia entre dos puntos geograficos
+#' @param lng1 longitud 1
+#' @param lat latitud 1
+#' @param lng2 longitud 2
+#' @param lat2 latitud 2
 calcularDistanciaEntreDosCoordenadas <- function(lng1, lat1, lng2, lat2) {
   rad <- pi/180
   a1 <- lat1 * rad
@@ -36,4 +44,18 @@ calcularDistanciaEntreDosCoordenadas <- function(lng1, lat1, lng2, lat2) {
   R <- 6378.145
   d <- R * c
   return(d)
+}
+
+#' Hace backup de las colecciones de base de datos en un directorio especificado
+#' @param directorio Nombre del directorio en el que se colocaran los archivos del backup
+backup <- function(directorio) {
+  STR_BASE_DATOS = "azucarDB"
+  colecciones = c('campanas', 'sesions', 'medicions', 'lotes', 'plantons', 'puntocaptacions')
+  for (colec in colecciones) {
+    myMongo <- mongo(db=STR_BASE_DATOS, colec)
+    dir.create(directorio, showWarnings = FALSE)
+    filepath <- paste(directorio, '/', colec, '.json', sep="")
+    f <- file(filepath)
+    exported <- myMongo$export(f)
+  }
 }
--- R/main.R
+++ R/main.R
@@ -2,5 +2,6 @@ library(mongolite)
 
 # Representa la constante de la gravedad
 GRAVEDAD = 9.81
+STR_BASE_DATOS = "azucarDB"
 
 source("./R/modelo.R")
